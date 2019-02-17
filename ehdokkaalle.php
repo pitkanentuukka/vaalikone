39c0fb563949b1652e87705e29e7e435eadbefcb
tuukka <devnull@localhost>
2019-02-17 16:10:17 +0200

    added candidate insertion

--- ehdokkaalle.php
+++ ehdokkaalle.php
@@ -6,76 +6,64 @@ include("sql.php");
 
 if (isset($_POST['submit'])) {
 
+
+	$puolueid = 1; // FIXME!!! tää on testausta varten
+	$vaalipiiriid = $mysqli->real_escape_string($_POST['vaalipiiri']);
+	$ehdokasnimi = $mysqli->real_escape_string($_POST['name']);
+	$numero = $mysqli->real_escape_string($_POST['numero']);
+	$insertehdokas = "INSERT INTO ehdokas (puolue, vaalipiiri, nimi, numero) VALUES ('$puolueid', '$vaalipiiriid',  '$ehdokasnimi', '$numero' )";
+	$mysqli->query($insertehdokas);
+	//$mysqli->begin_transaction();	
+/*try {	
+	$mysqli->query($insertehdokas) or throw new Exception('error');
+
+	}*/
+//	$mysqli->commit();
+
+
 } else {
-	// puolue otetaan get-parametrina, se pitää validoida
-	/*$link = $_GET['link'];
-	$query = $mysqli->prepare('SELECT * FROM puolue WHERE link = ?');
-	 */
 
-	//$get_questions = $mysqli->prepare('SELECT * FROM kysymys');
-
-	// Form for name, number, stuff
-
-
-echo "<div class='form'>";
-echo "<form action='ehdokkaalle.php' method='post'>";
-echo "<p>";
-echo "Nimi: <input type=text name='name'>";
-echo "</p>";
-// haetaan kannasta vaalipiirit, foreach
-$query = "SELECT * FROM vaalipiiri";
-$result = $mysqli->query($query);
-//$row = $result->fetch_array(MYSQLI_ASSOC);
-echo "Vaalipiiri: <select name='vaalipiiri'>";
-while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-
-	//echo "<option value=" . $vaalipiiri['id'] .">" . $Vaalipiiri['vaalipiiri'] ."</option>";
-	echo "<option value=" . $row['id'] .">" . $row['vaalipiiri'] ."</option>";
-	//var_dump($row);
-
-}
-echo "</select>";
-//$vaalipiiriarray = mysqli_fetch_all($result);
-/*echo "<select name='vaalipiiri' id='vaalipiiri_id'>";
-while ($row = mysqli_fetch_array($result))
-	echo "<p>$row</p>";
-/*
-	foreach ($vaalipiiriarray as $vaalipiiri) {
-
-
-}
-*/
-/*
-
-
-
-
-
-
- */
-$query = "SELECT * FROM kysymys";
-$result = $mysqli->query($query);
-
-while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+	echo "<div class='form'>";
+	echo "<form action='ehdokkaalle.php' method='post'>";
 	echo "<p>";
-	echo "<div>". $row['kysymys']."</div>";
-	echo "<div>";	
-	echo "<input type='radio' name=" . $row['id'];
-		echo " value='1'>vahvasti eri mieltä";
-	echo "<input type='radio' name=" . $row['id'];
-		echo " value='2'>eri mieltä";
-	echo "<input type='radio' name=" . $row['id'];
-		echo " value='3'>ei mitään mieltä";
-	echo "<input type='radio' name=" . $row['id'];
-		echo " value='4'>samaa mieltä";
-	echo "<input type='radio' name=" . $row['id'];
-		echo " value='5'>vahvasti samaa mieltä";
-	echo "</div>";	
-	echo "<div>";	
-	echo "Lisäkommenti: <textarea name=$row[id] rows=5 cols=40></textarea>";
-
-
+	echo "Nimi: <input type=text name='name'>";
 	echo "</p>";
-}
-echo "<input type=submit value=submit name='lähetä'>";
+	echo "<p>";
+	echo "Ehdokasnumero: <input type=text name='numero'>";
+	echo "</p>";
+	$query = "SELECT * FROM vaalipiiri";
+	$result = $mysqli->query($query);
+	echo "Vaalipiiri: <select name='vaalipiiri'>";
+	while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+
+		echo "<option value=" . $row['id'] .">" . $row['vaalipiiri'] ."</option>";
+
+	}
+	echo "</select>";
+
+	$query = "SELECT * FROM kysymys";
+	$result = $mysqli->query($query);
+
+	while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+		echo "<p>";
+		echo "<div>". $row['kysymys']."</div>";
+		echo "<div>";	
+		echo "<input type='radio' name=" . $row['id'];
+			echo " value='1'>vahvasti eri mieltä";
+		echo "<input type='radio' name=" . $row['id'];
+			echo " value='2'>eri mieltä";
+		echo "<input type='radio' name=" . $row['id'];
+			echo " value='3'>ei mitään mieltä";
+		echo "<input type='radio' name=" . $row['id'];
+			echo " value='4'>samaa mieltä";
+		echo "<input type='radio' name=" . $row['id'];
+			echo " value='5'>vahvasti samaa mieltä";
+		echo "</div>";	
+		echo "<div>";	
+		echo "Lisäkommenti: <textarea name=$row[id] rows=5 cols=40></textarea>";
+
+
+		echo "</p>";
+	}
+	echo "<input type=submit value='lähetä' name='submit'>";
 }
