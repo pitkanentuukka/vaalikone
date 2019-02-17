da0af8102abc8761e6941a5fee6cb9a234175bac
tuukka <devnull@localhost>
2019-02-17 14:03:22 +0200

    we now have a field for the candidate name, and a dropdown list of districts, also fixed strange bugs in connection

--- ehdokkaalle.php
+++ ehdokkaalle.php
@@ -1,7 +1,6 @@
 <?php
-
+error_reporting(E_ALL);
 include("sql.php");
-
 // if form is submitted, validate and save answers to database
 // otherwise, fetch all questions and create the form
 
@@ -9,9 +8,49 @@ if (isset($_POST['submit'])) {
 
 } else {
 	// puolue otetaan get-parametrina, se pitää validoida
-	$puolue = $_GET['puolue'];
-	$query = $mysqli->prepare('SELECT * FROM puolue WHERE puolue = ?');
+	/*$link = $_GET['link'];
+	$query = $mysqli->prepare('SELECT * FROM puolue WHERE link = ?');
+	 */
 
+	//$get_questions = $mysqli->prepare('SELECT * FROM kysymys');
+
+	// Form for name, number, stuff
+
+
+echo "<div class='form'>";
+echo "<form action='ehdokkaalle.php' method='post'>";
+echo "<p>";
+echo "Nimi: <input type=text name='name'>";
+echo "</p>";
+// haetaan kannasta vaalipiirit, foreach
+$query = "SELECT * FROM vaalipiiri";
+$result = $mysqli->query($query);
+//$row = $result->fetch_array(MYSQLI_ASSOC);
+echo "Vaalipiiri: <select name='vaalipiiri'>";
+while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+
+	//echo "<option value=" . $vaalipiiri['id'] .">" . $Vaalipiiri['vaalipiiri'] ."</option>";
+	echo "<option value=" . $row['id'] .">" . $row['vaalipiiri'] ."</option>";
+	//var_dump($row);
+
+}
+echo "</select>";
+//$vaalipiiriarray = mysqli_fetch_all($result);
+/*echo "<select name='vaalipiiri' id='vaalipiiri_id'>";
+while ($row = mysqli_fetch_array($result))
+	echo "<p>$row</p>";
+/*
+	foreach ($vaalipiiriarray as $vaalipiiri) {
+
+
+}
+*/
+
+
+
+
+
+	$query = "SELECT * FROM kysymys";
 
 
 }
--- sql.php
+++ sql.php
@@ -1,12 +1,11 @@
 <?php
-
 $username = 'vaalikone';
 $password = "DZuHe6pmsU49pS29";
 $db = "vaalikone_proto";
 $mysqli = new mysqli("localhost", $username, $password, $db);
 if($mysqli->connect_error) {
-	exit('Error connecting to database', $mysqli->connect_error); 
+	exit('Error connecting to database'); 
 }
-$mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli->set_charset("utf8mb4");
+/*$mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli->set_charset("utf8mb4");*/
 ?>
