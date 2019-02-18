48128a4c7bc129c28df416c6a0b94ec4ffb78e42
tuukka <devnull@localhost>
2019-02-18 23:00:06 +0200

    answers are now saved to the database

--- ehdokkaalle.php
+++ ehdokkaalle.php
@@ -6,7 +6,7 @@ include("sql.php");
 
 if (isset($_POST['submit'])) {
 	// we need to validate that all questions have been answered before we get here
-
+	// let's find a javascript library for that
 
 	// debug!!
 	//var_dump($_POST);die;
@@ -20,17 +20,42 @@ if (isset($_POST['submit'])) {
 	$ehdokasnimi = $mysqli->real_escape_string($_POST['name']);
 	$numero = $mysqli->real_escape_string($_POST['numero']);
 	$insertehdokas = "INSERT INTO ehdokas (puolue, vaalipiiri, nimi, numero) VALUES ('$puolueid', '$vaalipiiriid',  '$ehdokasnimi', '$numero' )";
-	/*$mysqli->begin_transaction();	
+	//$mysqli->begin_transaction();	
 	$mysqli->query($insertehdokas);
-	$ehdokas_id = $mysqli->insert_id;*/
 	$ehdokas_id = $mysqli->insert_id;
+	
+	for ($i=1;$i<=$question_count;$i++) {
+		//var_dump($i);
+		//var_dump($_POST['radios']);
+		$answers = $_POST['radios'];
+		//echo "<p>answers: ";
+		$answer = $mysqli->real_escape_string($answers[$i]);
+		//echo "</p><p>comments: ";
+		$comments = $_POST['comments'];
+		$comment = $mysqli->real_escape_string($comments[$i]);
+		//var_dump($comments[$i]);
+		//echo "</p> ";
+		//var_dump($_POST["radios"[$i]]);
+		
+		//$insertanswer = "INSERT INTO vastaus (, "
 
-	/*for ($i=1;$i=<$question_count;$i++) {
-		$insertanswer = "INSERT INTO vastaus (, "
+		$insert = "INSERT INTO vastaus (kysymysid,ehdokasid, vastaus, teksti) VALUES ('$i', '$ehdokas_id', '$answer', '$comment')";
+		$mysqli->query($insert);
 
 
 	}
-	 */
+	die;
+	//foreach ($_POST['radios'] as $answers) {
+	/*while ($i<
+		//var_dump($answers);
+		
+		$vastaus = $mysqli->real_escape_string($answers);
+		//var_dump($vastaus);
+		var_dump($_POST['comments'[$i]]);
+		$i++;
+
+	}*/
+	
 //	$mysqli->commit();
 
 
